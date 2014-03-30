e5cbeae14710f4924af338a145637b088a1ae616
Pavel Sorokin <pavel.sorokin@nerro.eu>
2014-03-30 22:02:21 +0200

    Add class and unit test for magic packet

---
+++ core/src/main/java/eu/nerro/wolappla/entity/MagicPacket.java
@@ -0,0 +1,40 @@
+package eu.nerro.wolappla.entity;
+
+/**
+ * The <i>magic packet</i> is a broadcast frame that is required to turn on computers through wake-on-lan technology.
+ * A magic packet format look like this:
+ * <table border="1">
+ * <thead>
+ * <tr>
+ * <th>Synchronization Stream</th>
+ * <th>Target MAC</th>
+ * <th>Password (optional)</th>
+ * </tr>
+ * </thead>
+ * <tbody>
+ * <tr>
+ * <td align="center">6</td>
+ * <td align="center">96</td>
+ * <td align="center">0, 4 or 6</td>
+ * </tr>
+ * </tbody>
+ * </table>
+ * <ul>
+ * <li>Synchronization Stream is defined as 6 bytes of <code>0xff</code>.</li>
+ * <li>Target MAC block contains 16 duplications of the IEEEaddress of the target, with no breaks or interruptions.</li>
+ * <li>Password field is optional, but if present, contains either 4 bytes or 6 bytes.</li>
+ * </ul>
+ */
+public class MagicPacket {
+  private final byte[] mSynchronizationStream = new byte[6];
+
+  public MagicPacket() {
+    for (int i = 0; i < mSynchronizationStream.length; i++) {
+      mSynchronizationStream[i] = (byte) 0xff;
+    }
+  }
+
+  public byte[] getSynchronizationStream() {
+    return mSynchronizationStream;
+  }
+}
---
+++ core/src/test/java/eu/nerro/wolappla/entity/MagicPacketTest.java
@@ -0,0 +1,35 @@
+package eu.nerro.wolappla.entity;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+public class MagicPacketTest {
+  @Test
+  public void getSynchronizationStream_shouldNotBeNull() {
+    MagicPacket magicPacket = new MagicPacket();
+    byte[] synchronizationStream = magicPacket.getSynchronizationStream();
+
+    assertThat(synchronizationStream, is(notNullValue()));
+  }
+
+  @Test
+  public void getSynchronizationStream_lengthShouldBeSix() {
+    MagicPacket magicPacket = new MagicPacket();
+    byte[] stream = magicPacket.getSynchronizationStream();
+
+    assertThat(stream.length, is(6));
+  }
+
+  @Test
+  public void getSynchronizationStream_shouldBeSixBytesOf0xff() {
+    MagicPacket magicPacket = new MagicPacket();
+    byte[] actualStream = magicPacket.getSynchronizationStream();
+    byte[] expectedStream = {(byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0xff};
+
+    assertThat(actualStream, equalTo(expectedStream));
+  }
+}
