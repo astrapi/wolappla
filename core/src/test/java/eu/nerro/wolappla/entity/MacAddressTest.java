bc786abc77f5ff7e7158633d309bae848909f666
Pavel Sorokin <pavel.sorokin@nerro.eu>
2014-03-31 01:43:53 +0200

    Fix typo in test class of mac address

--- core/src/test/java/eu/nerro/wolappla/entity/MacAddressTest.java
+++ core/src/test/java/eu/nerro/wolappla/entity/MacAddressTest.java
@@ -4,12 +4,12 @@ import org.junit.Test;
 
 public class MacAddressTest {
   @Test(expected = IllegalArgumentException.class)
-  public void valueOf_shouldThrowAIEIfAddressIsNull() {
+  public void valueOf_shouldThrowIAEIfAddressIsNull() {
     MacAddress.valueOf(null);
   }
 
   @Test(expected = IllegalArgumentException.class)
-  public void valueOf_shouldThrowAIEIfAddressArrayLengthIsNotSix() {
+  public void valueOf_shouldThrowIAEIfAddressArrayLengthIsNotSix() {
     byte[] invalidAddress = { 1, 2, 3, 4, 5 };
     MacAddress.valueOf(invalidAddress);
   }
