352fe6eeb773a5d977593c070a5d8930becca2de
Pavel Sorokin <pavel.sorokin@nerro.eu>
2014-04-07 20:52:37 +0200

    Add log helper to make logging more consistent

---
+++ main/src/main/java/eu/nerro/wolappla/util/LogUtils.java
@@ -0,0 +1,41 @@
+package eu.nerro.wolappla.util;
+
+import android.util.Log;
+
+import eu.nerro.wolappla.BuildConfig;
+
+/**
+ * Helper methods that make logging more consistent throughout the app.
+ */
+public class LogUtils {
+  private static final String LOG_PREFIX = "wolappla_";
+  private static final int LOG_PREFIX_LENGTH = LOG_PREFIX.length();
+  private static final int MAX_LOG_TAG_LENGTH = 25;
+
+  private LogUtils() {
+  }
+
+  public static String makeLogTag(String name) {
+    if (name.length() > MAX_LOG_TAG_LENGTH - LOG_PREFIX_LENGTH) {
+      return LOG_PREFIX + name.substring(0, MAX_LOG_TAG_LENGTH - LOG_PREFIX_LENGTH - 1);
+    }
+
+    return LOG_PREFIX + name;
+  }
+
+  public static String makeLogTag(Class clazz) {
+    return makeLogTag(clazz.getSimpleName());
+  }
+
+  public static void LOGD(final String tag, String message) {
+    if (BuildConfig.DEBUG || Log.isLoggable(tag, Log.DEBUG)) {
+      Log.d(tag, message);
+    }
+  }
+
+  public static void LOGD(final String tag, String message, Throwable cause) {
+    if (BuildConfig.DEBUG || Log.isLoggable(tag, Log.DEBUG)) {
+      Log.d(tag, message, cause);
+    }
+  }
+}
