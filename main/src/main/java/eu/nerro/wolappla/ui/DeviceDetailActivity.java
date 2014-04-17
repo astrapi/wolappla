95cf8cbd29fc3cb102f9d121c4633b8c6e35ef24
Pavel Sorokin <pavel.sorokin@nerro.eu>
2014-04-17 21:20:26 +0200

    Add device detail activity (wip)

---
+++ main/src/main/java/eu/nerro/wolappla/ui/DeviceDetailActivity.java
@@ -0,0 +1,14 @@
+package eu.nerro.wolappla.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import eu.nerro.wolappla.R;
+
+public class DeviceDetailActivity extends Activity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_device_detail);
+  }
+}
