4acc9bd7db5f0f33fef47da4d9d44ae7777ec132
Pavel Sorokin <pavel.sorokin@nerro.eu>
2014-04-19 02:05:05 +0200

    Change device fragment to list fragment from support library

--- main/src/main/java/eu/nerro/wolappla/ui/DevicesFragment.java
+++ main/src/main/java/eu/nerro/wolappla/ui/DevicesFragment.java
@@ -1,16 +1,21 @@
 package eu.nerro.wolappla.ui;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 import eu.nerro.wolappla.R;
 
-public class DevicesFragment extends Fragment {
+public class DevicesFragment extends ListFragment {
+  private String[] mDevices = { "One", "Two", "Three", "Four" };
+
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    ArrayAdapter<String> mAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, mDevices);
+    setListAdapter(mAdapter);
     return inflater.inflate(R.layout.fragment_list_devices, container, false);
   }
 }
