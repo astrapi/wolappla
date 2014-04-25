c4ae3c8f848bde8bc824a9456a17660ccb42c8c4
Pavel Sorokin <pavel.sorokin@nerro.eu>
2014-04-25 22:08:03 +0200

    Set home activity as parent for device detail activity

--- main/src/main/java/eu/nerro/wolappla/ui/DeviceDetailActivity.java
+++ main/src/main/java/eu/nerro/wolappla/ui/DeviceDetailActivity.java
@@ -1,7 +1,10 @@
 package eu.nerro.wolappla.ui;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
 
 import eu.nerro.wolappla.R;
 
@@ -9,6 +12,23 @@ public class DeviceDetailActivity extends Activity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
+    final ActionBar actionBar = getActionBar();
+    if (actionBar != null) {
+      actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
     setContentView(R.layout.activity_device_detail);
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case android.R.id.home:
+        NavUtils.navigateUpFromSameTask(this);
+        return true;
+    }
+
+    return super.onOptionsItemSelected(item);
   }
 }
